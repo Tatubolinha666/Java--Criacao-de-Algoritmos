9e094d94f78935abd07b2b34e5fd9c0f9c7a29e4
Tatubolinha666 <88353359+Tatubolinha666@users.noreply.github.com>
2021-09-21 22:11:15 -0300

    Add files via upload

---
+++ Sucessor.java
@@ -0,0 +1,23 @@
+
+package sucessor;
+
+import java.util.Scanner;
+
+
+public class Sucessor {
+
+
+    public static void main(String[] args) {
+static Scanner grava=new Scanner(System.in);
+static int  val1;
+public static void prox (){
+    int prox;
+System.out.println("Digite um valor e seu sucessor aparecerá");
+val1=grava.nextInt();
+prox=val1+1;
+System.out.println("O próximo número é:"+prox);
+
+}
+    }
+    
+
