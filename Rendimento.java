0b9933d2f6bd4f0b3e144e40b4f3780fbe86c06d
Tatubolinha666 <88353359+Tatubolinha666@users.noreply.github.com>
2021-09-23 00:22:46 -0300

    Add files via upload

--- Rendimento.java
+++ Rendimento.java
@@ -1,16 +1,21 @@
 
-package rendimento;
-
+import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
+
 public class Rendimento {
-static String x;
+    
+    static String x;
   
     public static void main(String[] args) {
+      
       x = JOptionPane.showInputDialog(null,"Digite o valor depositado");
 Double val1= Double.parseDouble(x);
 Double rend= val1*(0.7/100);
-JOptionPane.showMessageDialog(null, " Seu rendimento por mês foi de: " + rend+" reais");
+DecimalFormat formatador = new DecimalFormat("0.00");
+JOptionPane.showMessageDialog(null, " Seu rendimento por mês foi de "+formatador.format(rend)+" reais");
     }
     
 }
+
+   
